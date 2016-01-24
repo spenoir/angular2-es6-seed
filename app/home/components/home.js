7b92fa3c2a5fa56c2481c77c6c6b8d1e9c0608b9
Evan Plaice <evanplaice@gmail.com>
2016-01-24 09:30:58 -0800

    Add HomeCmp + template + css

---
+++ app/home/components/home.js
@@ -0,0 +1,8 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: './app/home/components/home.html',
+  styleUrls: [ './app/home/components/home.css' ]
+})
+export class HomeCmp {}
